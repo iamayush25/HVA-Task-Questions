9622a42e6b778ec46e3e8894924bfce992a14a14
Ayush Sharma <ayush22@navgurukul.org>
2024-04-23 23:39:14 +0530

    HVA coding task

---
+++ First_n _Even_Numbers.js
@@ -0,0 +1,8 @@
+let input = require('readline-sync')
+let number = input.questionInt('Enter Number :- ')
+for (let i = 1; i < number + 1; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+    
+}
