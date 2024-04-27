0bc4ffbe65ddb918a23b94f679a75c48bff4e2fd
Ayush Sharma <ayush22@navgurukul.org>
2024-04-28 00:07:42 +0530

    HVA Coding Task

---
+++ Multiplication_of_N.js
@@ -0,0 +1,7 @@
+const input = require('readline-sync')
+let num = input.questionInt('Enter The number  :- ')
+for (let i = num; i <= num; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(i ,"*" ,  j , "= ",i*j);
+    }
+}
