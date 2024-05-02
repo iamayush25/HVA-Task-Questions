cea0741c1fe72b8e21bd395fc2681a58a3a4584f
Ayush Sharma <ayush22@navgurukul.org>
2024-05-02 23:19:00 +0530

    HVA Coding Task

---
+++ Sum_of_digit.js
@@ -0,0 +1,7 @@
+let input = require('readline-sync')
+let number = input.question('Enter The Number : ')
+let sum = 0;
+for (let i = 0; i <= number.length; i++) {
+    sum = 
+    console.log(sum); 
+}
