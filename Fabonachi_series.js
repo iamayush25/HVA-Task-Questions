ddda900abb43d342d1900bdf4e7e23ddf820f3ca
Ayush Sharma <ayush22@navgurukul.org>
2024-05-03 21:41:25 +0530

    HVA Coding Task

---
+++ Fabonachi_series.js
@@ -0,0 +1,11 @@
+let input = require('readline-sync')
+let number = input.questionInt('Enter The Number : ')
+let a1 = 0;
+let a2 = 1;
+let a3 ;
+for (let i = 0; i <= number; i++) {
+    a3 = a1 + a2 ;
+    a1 = a2 ; 
+    a2 = a3
+    console.log(a3);
+}   
